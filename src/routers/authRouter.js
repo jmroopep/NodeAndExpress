fe381b9c5dbd8157a6c492bc6738a1b3638a743f
Janice Pepper <janice.pepper@accenture.com>
2024-06-26 20:12:11 -0600

    fixed bug in code

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -5,10 +5,31 @@ const { MongoClient, ObjectId } = require("mongodb");
 const authRouter = express.Router();
 
 authRouter.route("/signUp").post((req, res) => {
-  //TODO create user
-  req.login(req.body, () => {
-    res.redirect("/auth/profile");
-  });
+  const { username, password } = req.body;
+  const url =
+    "mongodb+srv://janeaquafina:PSofAKVbDXNtqowd@globomantics.84ky1vy.mongodb.net/?retryWrites=true&w=majority";
+  const dbName = "globomantics";
+
+  (async function addUser() {
+    let client;
+    try {
+      client = await MongoClient.connect(url);
+      const db = client.db(dbName);
+      const user = { username, password };
+      const results = await db.collection("users").insertOne(user);
+      debug(results);
+      const savedUser = await db
+        .collection("users")
+        .findOne({ _id: results.insertedId });
+      //now that user is created, log them in
+      req.login(savedUser, () => {
+        res.redirect("/auth/profile");
+      });
+    } catch (error) {
+      debug(error);
+    }
+    client.close();
+  })();
 });
 authRouter.route("/profile").get((req, res) => {
   res.json(req.user);
